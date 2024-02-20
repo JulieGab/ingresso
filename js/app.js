c51f80034793ce09ff05067a643dda9b7aa569db
JulieGab <jullygabriella.sa@gmail.com>
2024-02-20 20:29:09 -0300

    feat: verificando compra para tipo superior e inferior

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ function comprar() {
     let qtd = parseInt(document.getElementById('qtd').value);
 
     if(tipo.value == 'pista') {
-        comprarPista(qtd);
+        comprarPista(qtd); 
+    } else if(tipo.value == 'inferior'){
+        comprarInferior(qtd);
+    } else {
+        comprarSuperior(qtd);
     }
 }
 
@@ -14,6 +18,28 @@ function comprarPista(qtd) {
     } else { 
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra efetuada com sucesso');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para o tipo "inferior"');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra efetuada com sucesso')
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para o tipo "superior"');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
         alert('Compra efetuada com sucesso');
     }
 }
